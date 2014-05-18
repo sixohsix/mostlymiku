b1bbea67c83cd110f66cf03f6bdef0787c5fb274
Mike Verdone <mike.verdone@gmail.com>
2014-05-18 17:12:58 +0200

    Async load playlist

--- js/miku.js
+++ js/miku.js
@@ -1,40 +1,80 @@
 // miku.js
-window.Miku = (function (window, gapi) {
+window.Miku = (function (window) {
 
+var gapi;
 var q = window.document.querySelectorAll.bind(window.document);
+var l = Miku.logging ? window.console.log.bind(window.console) : function () {};
 
 var Miku = {};
+Miku.logging = true;
+
 Miku.api_key = "AIzaSyDsImZ2h077_LxeDhwh3Q6fjHtd5uZ7aaM";
-Miku.playlistId = "ON6xeP4BD9pU8hGN5O_yDrrBckEVQrR";
+Miku.playlistId = "PL_ON6xeP4BD9pU8hGN5O_yDrrBckEVQrR";
 Miku.playlistItems = [];
+Miku.asyncActions = [];
+
+var next = Miku.doNextAction = function () {
+	if (Miku.asyncActions.length === 0)
+		return;
+
+	var nextAction = Miku.asyncActions.shift();
+	window.setTimeout(nextAction, 0);
+}
+
+var acts = Miku.pushActions = function (actions) {
+	Miku.asyncActions = Miku.asyncActions.concat(actions);
+	Miku.doNextAction();
+}
+
+var act = Miku.pushAction = function (action) {
+	Miku.pushActions([action]);
+}
 
 Miku.onLoad = function () {
+	gapi = window.gapi;
 	gapi.client.setApiKey(Miku.api_key);
-	gapi.client.load("youtube", "v3", Miku.onYouTubeLoaded);
+	acts([
+		function () { 
+			l("Loading youtube");
+			gapi.client.load("youtube", "v3", next);
+		},
+		Miku.loadPlaylist,
+		Miku.dumpPl
+	])
 }
 
 Miku.loadPlaylist = function () {
 	var nextPageToken = "",
-		playlistItems;
-	while (nextPageToken !== undefined) {
-		var res = gapi.client.youtube.playlistitems.list({
-			playlistId: playlistId,
+		playlistItems = [];
+
+	function loadMore() {
+		var req = gapi.client.youtube.playlistItems.list({
+			playlistId: Miku.playlistId,
 			maxItems: 50,
 			part: "contentDetails",
 			pageToken: nextPageToken});
-		playlistItems = playlistItems.concat(res.items.map(function (item) {
-			return {videoId: item.contentDetails.videoId};
-		}));
-		nextPageToken = res.nextPageToken;
+		req.execute(function (res) {
+			playlistItems = playlistItems.concat(res.items.map(function (item) {
+				return {videoId: item.contentDetails.videoId};
+			}));
+			l("Loaded", req.items.length, "videos");
+			nextPageToken = res.nextPageToken;
+			if (nextPageToken !== undefined) {
+				loadMore();
+			}
+			else {
+				Miku.playlistItems = playlistItems;
+				next();
+			}
+		});
 	}
-	return playlistItems;
+	loadMore();
 }
 
-Miku.onYouTubeLoaded = function () {
-	Miku.playlistItems = Miku.loadPlaylist();
-	console.log("loaded the pl", Miku.playlistItems);
+Miku.dumpPl = function () {
+	l("Loaded the playlist", Miku.playlistItems);
+	next();
 }
-
 
 return Miku;
-}(window, window.gapi));
+}(window));
